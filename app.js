6ab322c2e667c8dfd0e0535326f6fb0d4b459281
ArchitBorde <archit.borde@gmail.com>
2022-02-22 23:07:32 +0530

    btn

---
+++ app.js
@@ -0,0 +1,11 @@
+//var asb = prompt("Whats your name?","Archit Borde");
+//alert("Whats Up!");
+
+var btnSelector = document.querySelector("#btn").addEventListener("click", function (){
+    console.log("you clicked me");
+});
+
+// var btn = document.getElementById("btnSelector").addEventListener("click",function(){
+//     console.log("You clicked me!");
+// });
+
